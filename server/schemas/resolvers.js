ea5025bec567c3309812ae0b78aea344f3ab3070
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-07-02 16:18:39 -0400

    set up typeDefs and resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,45 @@
+// TODO: define the query and mutation functionality to work with the Mongoose models
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      return User.findOne({ _id: context.user._id });
+    },
+  },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, args) => {
+      await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $addToSet: { savedBooks: args } },
+        { new: true, runValidators: true }
+      );
+    },
+  },
+};
+
+module.exports = resolvers;
