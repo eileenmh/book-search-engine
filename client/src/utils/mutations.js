22b2fb71ea4994f06b99718fd78407559a309261
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-07-05 23:16:20 -0400

    use useMutation to add user upon form submit

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -17,3 +17,14 @@ export const LOGIN_USER = gql`
     }
   }
 `;
+
+export const ADD_USER = gql`
+  mutation AddUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
