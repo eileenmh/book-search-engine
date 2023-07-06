d350155f9acdf422e2e1afabe7fa63641bfb722a
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-07-05 23:09:52 -0400

    use useMutation to login user on form submit

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,19 @@
+/* TODO: 
+- `LOGIN_USER` will execute the `loginUser` mutation set up using Apollo Server.
+- `ADD_USER` will execute the `addUser` mutation.
+- `SAVE_BOOK` will execute the `saveBook` mutation.
+- `REMOVE_BOOK` will execute the `removeBook` mutation.
+*/
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        email
+      }
+    }
+  }
+`;
